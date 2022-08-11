079c397073dec8ed1cc7a74945f1ca25ec37e7d2
Connor Dobson <dobco402@student.otago.ac.nz>
2022-08-11 03:58:14 +0000

    Changed setup-database.sql file Changed the sql scheme to add a new table called cinemaSessions to add showtime sessions for the movies in the database

--- setup-database.sql
+++ setup-database.sql
@@ -1,24 +1,33 @@
 CREATE TABLE movie (
-  movieName varchar(100) not null,
-  movieYear varchar(4) not null,
-  movieDirector varchar(100) not null,
-  cinemaRoom integer not null,
+  movieName varchar(100) NOT NULL,
+  movieYear YEAR NOT NULL,
+  movieDirector varchar(100) NOT NULL,
   primary key (movieName, movieYear)
 );
 
+CREATE TABLE cinemaSessions (
+  cinemaRoom integer NOT NULL,
+  movieName varchar(100) NOT NULL,
+  movieYear YEAR NOT NULL,
+  sessionDateTime DATETIME NOT NULL,
+  constraint fk_movie foreign key (movieName) references movie(movieName),
+  constraint fk_movie_year foreign key (movieYear) references movie(movieYear),
+  primary key (cinemaRoom, movieName, movieYear, sessionDateTime)
+);
+
 CREATE TABLE booking (
-  bookingID integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  bookingID integer PRIMARY KEY AUTO_INCREMENT,
   firstName varchar(50) NOT NULL,
   lastName varchar(50) NOT NULL,
   movieName varchar(100) NOT NULL,
-  movieYear varchar(4) NOT NULL,
+  movieYear YEAR NOT NULL,
   constraint fk_movie foreign key (movieName) references movie(movieName),
   constraint fk_movie_year foreign key (movieYear) references movie(movieYear)
 );
 
 AUTO_INCREMENT=1000;
 
-INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Spider-Man 3', '2007', 'Sam Raimi', 4);
-INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Doctor Stranger in the Multiverse of Madness, '2022', 'Sam Raimi', 1);
-INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Jurassic World Dominion', '2022', 'Colin Trevorrow', 2);
-INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Thor: Love and Thunder', '2022', 'Taika Waititi', 3);
+INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Spider-Man 3', '2007', 'Sam Raimi');
+INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Doctor Stranger in the Multiverse of Madness, '2022', 'Sam Raimi');
+INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Jurassic World Dominion', '2022', 'Colin Trevorrow');
+INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Thor: Love and Thunder', '2022', 'Taika Waititi');
