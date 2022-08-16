cf9723b8eec999149040c8277103e1e432d66af8
Connor Dobson <dobco402@student.otago.ac.nz>
2022-08-16 06:03:11 +0000

    Fixed setup-database.sql file I have made fixes to the sql script to add a booking table along with a sessionID to manage bookings more easier when managing different file sessions.

--- pos/PosClient.java
+++ pos/PosClient.java
@@ -3,15 +3,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class PosClient {
+    public static void main(String[] args) {
 	Connection conn = null;
-	public static void main(String[] args) {
-		try {
-			conn = DriverManager.getConnection("jdbc:mysql://192.168.56.12/hvlofi?user=posuser&password=posaccount");
-
-		System.out.println("HV LO-FI Point Of Sale Terminal");
-
-		System.out.println("Please Login");
-
-
+	try {
+	    Class.forName("com.mysql.jdbc.Driver");
+	    conn = DriverManager.getConnection("jdbc:mysql://192.168.56.12/hvlofi?user=posuser&password=posaccount");
+	    
+	    System.out.println("HV LO-FI Point Of Sale Terminal");
+	    
+	    System.out.println("Please Login");
+	    
+	    
+	} catch (SQLException e) {
+	    e.printStackTrace();
 	}
+    }
 }
--- setup-database.sql
+++ setup-database.sql
@@ -6,28 +6,29 @@ CREATE TABLE movie (
 );
 
 CREATE TABLE cinemaSessions (
+  sessionID integer PRIMARY KEY AUTO_INCREMENT,
   cinemaRoom integer NOT NULL,
   movieName varchar(100) NOT NULL,
   movieYear YEAR NOT NULL,
   sessionDateTime DATETIME NOT NULL,
-  constraint fk_movie foreign key (movieName) references movie(movieName),
-  constraint fk_movie_year foreign key (movieYear) references movie(movieYear),
-  primary key (cinemaRoom, movieName, movieYear, sessionDateTime)
+  constraint fk_movie_session foreign key (movieName, movieYear) references movie(movieName, movieYear)
 );
 
 CREATE TABLE booking (
   bookingID integer PRIMARY KEY AUTO_INCREMENT,
   firstName varchar(50) NOT NULL,
   lastName varchar(50) NOT NULL,
-  movieName varchar(100) NOT NULL,
-  movieYear YEAR NOT NULL,
-  constraint fk_movie foreign key (movieName) references movie(movieName),
-  constraint fk_movie_year foreign key (movieYear) references movie(movieYear)
+  sessionID integer NOT NULL,
+  constraint fk_session foreign key (sessionID) references cinemaSessions(sessionID)
 );
 
-AUTO_INCREMENT=1000;
+ALTER TABLE cinemaSessions AUTO_INCREMENT=100;
+ALTER TABLE booking AUTO_INCREMENT=100;
 
-INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Spider-Man 3', '2007', 'Sam Raimi');
-INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Doctor Stranger in the Multiverse of Madness, '2022', 'Sam Raimi');
-INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Jurassic World Dominion', '2022', 'Colin Trevorrow');
-INSERT INTO movie (movieName, movieYear, movieDirector, cinemaRoom) VALUES ('Thor: Love and Thunder', '2022', 'Taika Waititi');
+INSERT INTO movie (movieName, movieYear, movieDirector) VALUES ('Spider-Man 3', 2007, 'Sam Raimi');
+INSERT INTO movie (movieName, movieYear, movieDirector) VALUES ('Doctor Strange in the Multiverse of Madness', 2022, 'Sam Raimi');
+INSERT INTO movie (movieName, movieYear, movieDirector) VALUES ('Jurassic World Dominion', 2022, 'Colin Trevorrow');
+INSERT INTO movie (movieName, movieYear, movieDirector) VALUES ('Thor: Love and Thunder', 2022, 'Taika Waititi');
+
+INSERT INTO cinemaSessions (cinemaRoom, movieName, movieYear, sessionDateTime) VALUES (4, 'Doctor Strange in the Multiverse of Madness', 2022, '2022-08-22 10:30:00');
+INSERT INTO cinemaSessions (cinemaRoom, movieName, movieYear, sessionDateTime) VALUES (1, 'Thor: Love and Thunder', 2022, '2022-08-22 11:30:00');
